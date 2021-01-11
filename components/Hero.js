6c6953b8304a22b1993c9eb0e922d655d86b5a38
Andrew Smith <andrew.smith@webmatters.io>
2021-01-10 21:59:05 -0500

    Adds styles sheet for active button class.

--- components/Hero.js
+++ components/Hero.js
@@ -248,7 +248,7 @@ export default function Hero() {
                   <div className="rounded-md shadow">
                     <a
                       href="#"
-                      className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-700 md:py-4 md:text-lg md:px-10"
+                      className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-700 md:py-4 md:text-lg md:px-10 btn"
                     >
                       Schedule Now
                     </a>
@@ -256,7 +256,7 @@ export default function Hero() {
                   <div className="mt-3 sm:mt-0 sm:ml-3">
                     <a
                       href="#"
-                      className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-blue-700 bg-blue-100 hover:bg-blue-200 md:py-4 md:text-lg md:px-10"
+                      className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-blue-700 bg-blue-100 hover:bg-blue-200 md:py-4 md:text-lg md:px-10 btn"
                     >
                       Learn More
                     </a>
