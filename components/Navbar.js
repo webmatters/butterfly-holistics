6266dc9fdfad4c7266b807ea6252ae03f50a24c6
Andrew Smith <andrew.smith@webmatters.io>
2021-01-23 11:49:31 -0500

    Deletes unneeded files. Adds covid banner and page.

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import Link from 'next/link'
 import { Transition } from '@headlessui/react'
+import CovidBanner from './CovidBanner'
 
-function Navbar(props) {
-  const [isOpen, setIsOpen] = useState(false)
+function Navbar() {
+  const [navOpen, setNavOpen] = useState(false)
+  const [bannerOpen, setBannerOpen] = useState(true)
 
   const navLinks = [
     { id: '1', path: '/about', name: 'About' },
@@ -40,56 +42,60 @@ function Navbar(props) {
 
   return (
     <div className="relative bg-white">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center  py-4 md:space-x-10">
-          {/* Brand logo */}
-          <div className="flex justify-start lg:w-0 lg:flex-1">
-            <Link href="/">
-              <a>
-                <span className="sr-only">Butterfly Holistics</span>
+      <div className="max-w-7xl mx-auto ">
+        {bannerOpen && (
+          <CovidBanner toggleBanner={setBannerOpen} bannerOpen={bannerOpen} />
+        )}
+        <div className="px-4 sm:px-6 lg:px-8">
+          <div className="flex justify-between items-center  py-4 md:space-x-10">
+            {/* Brand logo */}
+            <div className="flex justify-start lg:w-0 lg:flex-1">
+              <Link href="/">
+                <a>
+                  <span className="sr-only">Butterfly Holistics</span>
 
-                <img
-                  className="hidden lg:inline h-20 w-20 mr-3"
-                  src="butterfly-logo-icon.png"
-                  alt="Butterfly Icon"
-                />
-                <img
-                  className="inline h-14 w-auto "
-                  src="butterfly-logo-words.png"
-                  alt="Butterfly Holistics logo"
-                />
-              </a>
-            </Link>
-          </div>
+                  <img
+                    className="hidden lg:inline h-20 w-20 mr-3"
+                    src="butterfly-logo-icon.png"
+                    alt="Butterfly Icon"
+                  />
+                  <img
+                    className="inline h-14 w-auto "
+                    src="butterfly-logo-words.png"
+                    alt="Butterfly Holistics logo"
+                  />
+                </a>
+              </Link>
+            </div>
 
-          {/* hamburger menu to open Mobile Nav */}
-          <div className="-mr-2 -my-2 sm:hidden">
-            <button
-              onClick={() => setIsOpen(!isOpen)}
-              type="button"
-              className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500"
-            >
-              <span className="sr-only">Open menu</span>
-              {/* Heroicon name: menu */}
-              <svg
-                className="h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
+            {/* hamburger menu to open Mobile Nav */}
+            <div className="-mr-2 -my-2 sm:hidden">
+              <button
+                onClick={() => setNavOpen(!navOpen)}
+                type="button"
+                className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            </button>
-          </div>
+                <span className="sr-only">Open menu</span>
+                {/* Heroicon name: menu */}
+                <svg
+                  className="h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              </button>
+            </div>
 
-          {/*
+            {/*
         Mobile menu, show/hide based on mobile menu state.
     
         Entering: "duration-200 ease-out"
@@ -99,70 +105,70 @@ function Navbar(props) {
           From: "opacity-100 scale-100"
           To: "opacity-0 scale-95"
       */}
-          <Transition
-            show={isOpen}
-            enter="ease-out duration-150"
-            enterFrom="opacity-0 scale-95"
-            enterTo="opacity-100 scale-100"
-            leave="ease-in duration-100"
-            leaveFrom="opacity-100 scale-100"
-            leaveTo="opacity-0 scale-95"
-          >
-            <div className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden z-10">
-              <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
-                <div className="pt-5 pb-6 px-5">
-                  <div className="flex items-center justify-between">
-                    <div className="flex justify-start lg:w-0 lg:flex-1">
-                      <Link href="/">
-                        <a>
-                          <span className="sr-only">Butterfly Holistics</span>
+            <Transition
+              show={navOpen}
+              enter="ease-out duration-150"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-100"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <div className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden z-10">
+                <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
+                  <div className="pt-5 pb-6 px-5">
+                    <div className="flex items-center justify-between">
+                      <div className="flex justify-start lg:w-0 lg:flex-1">
+                        <Link href="/">
+                          <a>
+                            <span className="sr-only">Butterfly Holistics</span>
 
-                          <img
-                            className="hidden md:inline h-20 w-20 mr-3"
-                            src="butterfly-logo-icon.png"
-                            alt="Butterfly icon"
-                          />
-                          <img
-                            className="md:inline h-14 w-auto "
-                            src="butterfly-logo-words.png"
-                            alt="Butterfly Holistics logo"
-                          />
-                        </a>
-                      </Link>
-                    </div>
-                    <div className="-mr-2">
-                      <button
-                        onClick={() => setIsOpen(!isOpen)}
-                        type="button"
-                        className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500"
-                      >
-                        <span className="sr-only">Close menu</span>
-                        {/* Heroicon name: x */}
-                        <svg
-                          className="h-6 w-6"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                          aria-hidden="true"
+                            <img
+                              className="hidden md:inline h-20 w-20 mr-3"
+                              src="butterfly-logo-icon.png"
+                              alt="Butterfly icon"
+                            />
+                            <img
+                              className="md:inline h-14 w-auto "
+                              src="butterfly-logo-words.png"
+                              alt="Butterfly Holistics logo"
+                            />
+                          </a>
+                        </Link>
+                      </div>
+                      <div className="-mr-2">
+                        <button
+                          onClick={() => setNavOpen(!navOpen)}
+                          type="button"
+                          className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500"
                         >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M6 18L18 6M6 6l12 12"
-                          />
-                        </svg>
-                      </button>
+                          <span className="sr-only">Close menu</span>
+                          {/* Heroicon name: x */}
+                          <svg
+                            className="h-6 w-6"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            aria-hidden="true"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M6 18L18 6M6 6l12 12"
+                            />
+                          </svg>
+                        </button>
+                      </div>
+                    </div>
+                    <div className="mt-6">
+                      <nav className="grid gap-y-8">{renderMobileLinks()}</nav>
                     </div>
                   </div>
-                  <div className="mt-6">
-                    <nav className="grid gap-y-8">{renderMobileLinks()}</nav>
-                  </div>
-                </div>
 
-                {/* Mobile contact button */}
-                {/* <div className="py-6 px-5 space-y-6">
+                  {/* Mobile contact button */}
+                  {/* <div className="py-6 px-5 space-y-6">
                   <div>
                     <Link href="/contact">
                       <a className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-lg shadow-sm text-base font-medium text-white bg-primary hover:bg-primary-dark">
@@ -171,15 +177,16 @@ function Navbar(props) {
                     </Link>
                   </div>
                 </div> */}
+                </div>
               </div>
-            </div>
-          </Transition>
+            </Transition>
 
-          {/* Desktop Nav */}
-          <div className="hidden sm:block">
-            <nav className="-mb-px flex space-x-8" aria-label="Tabs">
-              {renderLinks()}
-            </nav>
+            {/* Desktop Nav */}
+            <div className="hidden sm:block">
+              <nav className="-mb-px flex space-x-8" aria-label="Tabs">
+                {renderLinks()}
+              </nav>
+            </div>
           </div>
         </div>
       </div>
---
+++ pages/covid.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import PageLayout from 'components/PageLayout'
+
+function Covid() {
+  return (
+    <PageLayout>
+      <div>
+        <h1>Covid page</h1>
+      </div>
+    </PageLayout>
+  )
+}
+
+export default Covid
