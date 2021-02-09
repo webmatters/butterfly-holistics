01aa59b5b8d9084f17d9e893c65e3a1c9b16fc3b
Andrew Smith <andrew.smith@webmatters.io>
2021-02-09 10:06:16 -0500

    Adds new Testimonal component and new testimonials.

--- components/Testimonials.js
+++ components/Testimonials.js
@@ -1,91 +1,53 @@
-export default function Testimonials(props) {
+import Testimonial from './Testimonial'
+
+const testimonials = [
+  {
+    id: 1,
+    text:
+      'Valerie is wonderful! My family and I live in NJ and have been doing virtual sessions. I suffer from autoimmune diseases and she has been a Godsend in helping with my chronic pain.',
+    image: 'Lorraine.jpg',
+    name: 'Lorraine G.',
+    location: 'New Jersey',
+  },
+  {
+    id: 2,
+    text:
+      'Working consistently with Valerie is one of the TOP TWO things I did for my business and myself personally... Before seeing Valerie, I was able to work on my mindset and know things intellectually but I was still struggling, so I knew the blocks and areas that needed help were on the subconscious and energetic               level. Valerie helps me clear these blocks so I can fully integrate and be whole and free from all the junk holding me back. I now have a sense of calm, groundedness, and success that I’ve never experienced before in my life.',
+    image: 'Sarah_M.jpg',
+    name: 'Sarah M.',
+    location: 'Holly Springs, NC',
+  },
+  {
+    id: 3,
+    text:
+      'I recently attended two workshops with Butterfly Holistics. Valerie is amazing, so knowledgeable about the human body and a healer. Not only did I learn specific skills to incorporate into my massage practice, I also benefited from the work as we practiced during class. I am better for attending, body, mind and spirit and will take more classes from her.',
+    image: 'Helen.jpg',
+    name: 'Helen R.',
+    location: 'Holly Springs, NC',
+  },
+  {
+    id: 4,
+    text:
+      "Valerie has truly changed my life through our regular sessions. She is able to help me narrow down what's going on and provides ways to truly heal from the inside out. I always feel supported, heard and connected. With her help I have been able to let go of pain-inducing habits and mindsets and really find myself again. I can't say enough about how much our sessions have truly impacted my life for the better!",
+    image: 'Kate.jpg',
+    name: 'Kate M.',
+    location: 'Holly Springs, NC',
+  },
+]
+
+export default function Testimonials() {
   return (
     <section className="bg-primary">
-      <div className="max-w-7xl mx-auto md:grid md:grid-cols-2 md:px-6 lg:px-16 text-center">
-        <div className="pt-6 lg:py-12 px-4 sm:px-6 md:flex md:flex-col md:py-16 md:pl-0 md:pr-10 md:border-r md:border-primary-dark lg:pr-16">
-          <blockquote className="mt-6 md:flex-grow md:flex md:flex-col">
-            <div className="relative text-base font-medium text-white md:flex-grow">
-              <svg
-                className="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 text-primary-light"
-                fill="currentColor"
-                viewBox="0 0 32 32"
-                aria-hidden="true"
-              >
-                <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
-              </svg>
-              <p className="relative">
-                Working consistently with Valerie is one of the TOP TWO things I
-                did for my business and myself personally... Before seeing
-                Valerie, I was able to work on my mindset and know things
-                intellectually but I was still struggling, so I knew the blocks
-                and areas that needed help were on the subconscious and
-                energetic level. Valerie helps me clear these blocks so I can
-                fully integrate and be whole and free from all the junk holding
-                me back. I now have a sense of calm, groundedness, and success
-                that I’ve never experienced before in my life..
-              </p>
-            </div>
-            <footer className="mt-8">
-              <div className="flex justify-center">
-                <div className="flex-shrink-0 inline-flex rounded-full border-2 border-white">
-                  <img
-                    className="h-12 w-12 rounded-full"
-                    src="Sarah_M.jpg"
-                    alt="Sarah M."
-                  />
-                </div>
-                <div className="ml-4">
-                  <div className="text-base font-medium text-white">
-                    Sarah M.
-                  </div>
-                  <div className="text-base font-medium text-primary-lightest">
-                    Holly Springs, NC
-                  </div>
-                </div>
-              </div>
-            </footer>
-          </blockquote>
-        </div>
-        <div className="py-6 px-4 border-t-2 border-primary sm:px-6 md:py-16 md:pr-0 md:pl-10 md:border-t-0 md:border-l lg:pl-16">
-          <blockquote className="mt-6 md:flex-grow md:flex md:flex-col">
-            <div className="relative text-base font-medium text-white md:flex-grow">
-              <svg
-                className="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 text-primary-light"
-                fill="currentColor"
-                viewBox="0 0 32 32"
-              >
-                <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
-              </svg>
-              <p className="relative">
-                I recently attended two workshops with Butterfly Holistics.
-                Valerie is amazing, so knowledgeable about the human body and a
-                healer. Not only did I learn specific skills to incorporate into
-                my massage practice, I also benefited from the work as we
-                practiced during class. I am better for attending, body, mind
-                and spirit and will take more classes from her.
-              </p>
-            </div>
-            <footer className="mt-8">
-              <div className="flex justify-center">
-                <div className="flex-shrink-0 inline-flex rounded-full border-2 border-white">
-                  <img
-                    className="h-12 w-12 rounded-full"
-                    src="Helen.jpg"
-                    alt="Helen R."
-                  />
-                </div>
-                <div className="ml-4">
-                  <div className="text-base font-medium text-white">
-                    Helen R.
-                  </div>
-                  <div className="text-base font-medium text-primary-lightest">
-                    Holly Springs, NC
-                  </div>
-                </div>
-              </div>
-            </footer>
-          </blockquote>
-        </div>
+      <div className="max-w-7xl mx-auto pb-12 md:pb-16 md:grid md:grid-cols-2 md:px-6 lg:px-16 text-center">
+        {testimonials.map(testimonial => (
+          <Testimonial
+            key={testimonial.id}
+            text={testimonial.text}
+            image={testimonial.image}
+            name={testimonial.name}
+            location={testimonial.location}
+          />
+        ))}
       </div>
     </section>
   )
